9e480a84e27d9d64e13e2a7d96d54877d6e20fcb
Rosa Rubio <paulina.rubio.p@gmail.com>
2024-08-23 18:14:38 -0400

    Creación archivo Ingredintes

---
+++ ingredientes.py
@@ -0,0 +1,19 @@
+# ingredientes.py
+
+"""
+Este módulo contiene listas de ingredientes posibles para las pizzas.
+
+Listas:
+    ingredientes_proteicos (list): Lista de ingredientes proteicos posibles.
+    ingredientes_vegetales (list): Lista de ingredientes vegetales posibles.
+    tipos_de_masa (list): Lista de tipos de masa posibles.
+"""
+
+# Lista de ingredientes proteicos posibles
+ingredientes_proteicos = ["pollo", "vacuno", "carne vegetal"]
+
+# Lista de ingredientes vegetales posibles
+ingredientes_vegetales = ["tomate", "aceitunas", "champiñones"]
+
+# Lista de tipos de masa posibles
+tipos_de_masa = ["tradicional", "delgada"]
